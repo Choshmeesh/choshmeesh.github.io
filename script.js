a1f60434c027389b31bce3e09f314388dd038911
Choshmeesh <127733184+Choshmeesh@users.noreply.github.com>
2025-08-09 00:45:56 +0000

    commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "vw";
 
-
+/*
 //using JavaScript to resize iframe based on content of snake game
 var iframe = document.getElementById('snaker');
 iframe.onload = function() {
     var iframeDocument = iframe.contentWindow.document.documentElement;
     iframe.style.height = iframeDocument.scrollHeight + 'px';
 };
+*/
