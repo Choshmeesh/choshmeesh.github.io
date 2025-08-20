fb54ab23f788474aa625af64914f4f9fcb57e79f
Choshmeesh <127733184+Choshmeesh@users.noreply.github.com>
2025-08-20 06:22:35 +0000

    commit

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "vw";
 
+//prevent scrolling when gaming:
+window.addEventListener('keydown', function (e) {
+  const arrows = ['ArrowLeft','ArrowUp','ArrowRight','ArrowDown'];
+  if (arrows.includes(e.key)) {
+    e.preventDefault(); // stop page scroll
+  }
+}, { passive: false });
+
 /*
 //using JavaScript to resize iframe based on content of snake game
 var iframe = document.getElementById('snaker');
